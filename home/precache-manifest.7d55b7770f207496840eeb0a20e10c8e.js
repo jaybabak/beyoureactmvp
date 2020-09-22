a4fc2730345eb3758a7158a8eb3140a7c89d51c1
Sami M <sami@starship-nginx-server.com>
2020-09-22 15:32:28 +0000

    Latest version live 4.

---
+++ home/precache-manifest.7d55b7770f207496840eeb0a20e10c8e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ffcf90a88e0b864fb2c7e539ea5e53",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "626019a59f1a23427532",
+    "url": "/home/static/css/2.5466c348.chunk.css"
+  },
+  {
+    "revision": "bd903dad3ce6b99cd1cc",
+    "url": "/home/static/css/main.7165e4da.chunk.css"
+  },
+  {
+    "revision": "626019a59f1a23427532",
+    "url": "/home/static/js/2.204fc186.chunk.js"
+  },
+  {
+    "revision": "bd903dad3ce6b99cd1cc",
+    "url": "/home/static/js/main.4be58d29.chunk.js"
+  },
+  {
+    "revision": "4e1ec8947a6857600002",
+    "url": "/home/static/js/runtime~main.0990ee84.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/home/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/home/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/home/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/home/static/media/slick.f97e3bbf.svg"
+  }
+]);
