9d5669c5d327cb4c4d8f9db85b8fac1bef8e36bc
Sami Malik <sami.malik@appnovation.com>
2020-09-28 17:46:51 -0400

    Updating back-end for sending emails.

--- app/index.js
+++ app/index.js
@@ -12,23 +12,61 @@ app.get('/app', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/app/send/email', (req, res) => {
-
-  const msg = {
-    to: 'sami@phot0x.com',
-    from: 'contact@mariamqureshi.com',
-    subject: 'Sending with Twilio SendGrid is Fun',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-  };
-  sgMail.send(msg);
-
-
-  res.send(process.env.SENDGRID_API_KEY);
-
-
-
+app.post('/app/send/email', (req, res) => {
+  if(req.headers.hasOwnProperty('authorization') && req.headers.authorization === 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9'){
+    if(req.body.hasOwnProperty('name') && req.body.hasOwnProperty('email') && req.body.hasOwnProperty('subject') && req.body.hasOwnProperty('message')){
+      try {
+        // To client
+        const msg = {
+          to: req.body.email,
+          from: 'contact@mariamqureshi.com',
+          subject: 'Be You by MQ | Mariam Qureshi - Contact Form',
+          html: `
+            <h2><strong>${req.body.name},</strong></h2>
+            <p>I have received your message and would like to thank you for writing to me. If your inquiry is urgent, please use the telephone number listed below to get in touch. Otherwise, I will reply by email as soon as possible.</p> 
+            <p>Talk to you soon,</p>
+            <p><strong>Be You by MQ | Mariam Qureshi</strong></p>
+            <p style="line-height: 1.2; word-break: break-word; mso-line-height-alt: 14px; margin: 0;">Mobile: 613.218.0587<br/>Email: contact@mariamqureshi.com<br/>Website: http://mariamqureshi.com</p>
+          `,
+        };
+        sgMail.send(msg);
+  
+        // To Mariam
+        const msgToMQ = {
+          to: 'contact@mariamqureshi.com',
+          from: 'contact@mariamqureshi.com',
+          subject: 'Contact form submission - Be You by MQ | Mariam Qureshi',
+          html: `
+            <strong>${req.body.name} just tried to reach you!</strong>
+            <br/>
+            <p>Contacts email: ${req.body.email}</p>
+            <p>Contacts subject: ${req.body.subject}</p>
+            <p>Contacts message: ${req.body.message}</p>
+          `,
+        };
+  
+        sgMail.send(msgToMQ);
+  
+        res.json({
+          sucess: true,
+          message: 'Message successfully delivered!'
+        });
+      
+      }catch(e){
+        console.log(e);
+        res.json({
+          sucess: false,
+          message: 'Message not delivered!'
+        });
+      }
+    }else {
+      return res.send(401);
+    }
+  }else {
+    return res.send(401);
+  }
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`MQ back-end app listening at http://localhost:${port}`)
 })
